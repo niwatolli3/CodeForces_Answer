cecf6fac1598919b440b2407e94c64b5a45c7ce3
niwatolli3 <niwatolli3@gmail.com>
2016-10-04 23:35:21 +0900

    723-D AC

--- src/Codeforces/723/D/Main.java
+++ src/Codeforces/723/D/Main.java
@@ -1,90 +1,135 @@
 import javafx.util.Pair;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-class Main {
+public final class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
-		Solver solver = new Solver();
-
-		final char CENTINEL = '_';
-		char[][] map = new char[60][60];
-		for(int i=0;i<60;i++) {
-			for(int j=0;j<60;j++) {
-				map[i][j] = CENTINEL;
-			}
-		}
-
-		int n, m, k;
-
-		n = in.nextInt();
-		m = in.nextInt();
-		k = in.nextInt();
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int k = in.nextInt();
+		Solver solver = new Solver(n,m,k);
 		in.nextLine();
 		for(int i=1;i<=n;i++) {
 			String s = in.nextLine();
 			for(int j=1;j<=m;j++) {
-					map[i][j] = s.charAt(j-1);
+				solver.setMapChar(i,j,s.charAt(j-1));
 			}
 		}
-
-		for(int i=0;i<=n;i++) {
-			for(int j=0;j<=m;j++) {
-				System.out.print(map[i][j]);
-			}
-			System.out.println("");
-		}
-
-		// 湖の１ first: pos(x,y), second: size of lake
-		List<Pair<Pair<Integer, Integer>, Integer>> lakes = new ArrayList<Pair<Pair<Integer, Integer>, Integer>>();
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n;j++) {
-				if(map[i][j] == '.') {
-					int size = searchLake(map, i, j, 0, 0);
-					if(size != 0) { // lake found
-						System.out.println(size);
-					}
-				}
-			}
-		}
-
-		for(int i=0;i<=n;i++) {
-			for(int j=0;j<=m;j++) {
-				System.out.print(map[i][j]);
-			}
-			System.out.println("");
-		}
-	}
-
-	/**
-	 * return is lake?
-	 * @param map
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public static int searchLake(char[][] map, int x, int y, int size, int around) {
-		if(map[x][y] != '.') return size;
-		map[x][y] = '*';
-		size += 1;
-
-		int dx[] = {-1,0,0,1};
-		int dy[] = {0,-1,1,0};
-		for(int i=0;i<4;i++) {
-			if(map[x+dx[i]][y+dy[i]] == '.')
-				around |= (1 << i);
-		}
-		int ans = 0;
-		for(int i=0;i<4;i++) {
-			ans += searchLake(map, x + dx[i], y + dy[i], size, around);
-		}
-		return ans;
+		solver.solve();
 	}
 }
 
 class Solver {
+	static char CENTINEL = '-';
+	static char LAKE = '.';
+	static char LAND = '*';
+	char[][] map = new char[52][52];
+    boolean[][] used = new boolean[52][52];
+	int n, m, k;
+    int around = 0;
+    boolean isLake = true;
+
+    int[] dx = {-1,0,0,1};
+    int[] dy = {0,-1,1,0};
+
+	int numRep = 0;
+	public Solver(int n, int m, int k) {
+		this.n = n;
+		this.m = m;
+		this.k = k;
+
+		for(int i=0;i<52;i++) {
+			for(int j=0;j<52;j++) {
+				map[i][j] = CENTINEL;
+                used[i][j] = false;
+			}
+		}
+	}
+
+	public void setMapChar(int x, int y, char ch) {
+		map[x][y] = ch;
+	}
+
+	public void solve() {
+        // (x,y), sizeOfLake
+		List<Pair<Pair<Integer, Integer>, Integer>> lakes = new ArrayList<Pair<Pair<Integer, Integer>, Integer>>();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=m;j++) {
+				if(used[i][j] == false && map[i][j] == LAKE) {
+                    around = 0;
+                    isLake = true;
+					int sizeOfLake = bfs(i,j);
+                    if(isLake) {
+                        Pair<Integer, Integer> xy = new Pair<>(i, j);
+                        lakes.add(new Pair<Pair<Integer, Integer>, Integer>(xy, sizeOfLake));
+                    }
+				}
+			}
+		}
+
+		Collections.sort(lakes, new Comparator<Pair<Pair<Integer, Integer>, Integer>>() {
+            @Override
+            public int compare(Pair<Pair<Integer, Integer>, Integer> o1, Pair<Pair<Integer, Integer>, Integer> o2) {
+                return o1.getValue() - o2.getValue();
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return false;
+            }
+        });
+
+        int num = lakes.size() - k;
+        int numChanged = 0;
+        for(int i=0;i<num;i++) {
+            numChanged += lakes.get(i).getValue();
+            fillLakeToLand(lakes.get(i).getKey().getKey(), lakes.get(i).getKey().getValue());
+        }
+
+        System.out.println(numChanged);
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    public void fillLakeToLand(int x, int y) {
+        if(map[x][y] != LAKE) return;
+        map[x][y] = LAND;
+
+        for(int i=0;i<4;i++) {
+            fillLakeToLand(x+dx[i], y+dy[i]);
+        }
+    }
+
+	public int bfs(int x, int y) {
+        if(used[x][y] == true) return 0;
+        if(map[x][y] == CENTINEL) isLake = false;
+        if(map[x][y] != LAKE) return 0;
+
+        used[x][y] = true;
+
+
+        int ans = 1;
+        for(int i=0;i<4;i++) {
+            int nextX = x+dx[i];
+            int nextY = y+dy[i];
+            if(map[nextX][nextY] == LAND) {
+                around |= (1 << i);
+            }
+            ans += bfs(nextX, nextY);
+        }
+        return ans;
+	}
+
+	// 8+4+2+1
+	public boolean isLake() {
+        return isLake;
+    }
 }
 
